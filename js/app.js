7ef34d4a10077d913ca658534ecb24715dbb7fce
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-09-10 13:32:56 -0400

    l2/2 - callback to display data

--- js/app.js
+++ js/app.js
@@ -5,9 +5,21 @@
 
   // modern js function syntax.  assign a variable to an anonymous function using a fat arrow =>
   let Start = () => {
-        console.log('App Started');
-        let x = 1;
-        console.log(x);
+      console.log('App Started');
+      let x = 1;
+      console.log(x);
+
+      // fetch & show contacts
+      getContacts((data) => {
+        let list = document.getElementById('contactList');
+
+        // create a new listItem for each contact
+        data.forEach(contact => {
+          let listItem = document.createElement('li');
+          listItem.innerText = contact.Name;
+          list.appendChild(listItem);
+        });
+      });        
   }; 
     
     // run the function
@@ -27,3 +39,12 @@ let updateCounter = (() => {
   }
   
 })();
+
+let getContacts = (callback) => {
+  // use jquery to read then display our json file contents
+  // the data param gets filled once all the data is read from the file
+  $.getJSON('./data/contacts.json', (data) => {
+    console.log(data);
+    callback(data);
+  });
+}
