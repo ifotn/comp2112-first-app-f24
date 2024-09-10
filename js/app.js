ad3a22939f9dd8fc11068d3a537c1215d3f2a384
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-09-10 12:47:43 -0400

    l2/1 - closure & contact setup

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
 // create an Immediately Invoked Function Expression (IIFE) to run at startup
 (function() {
-  function Start() {
+  // old js function syntax
+  // function Start() {
+
+  // modern js function syntax.  assign a variable to an anonymous function using a fat arrow =>
+  let Start = () => {
         console.log('App Started');
         let x = 1;
         console.log(x);
-    }; 
+  }; 
     
     // run the function
     window.addEventListener('load', Start);
     //console.log(x); this causes an Undefined error.  x only lives inside Start()
 }
 )();
+
+let updateCounter = (() => {
+  // now counter gets created and ininitalized to zero only once
+  let counter = 0;
+
+  return() => {
+    // every click can use the same counter var now
+    counter++;
+    document.getElementById('counter').innerHTML = counter;
+  }
+  
+})();
